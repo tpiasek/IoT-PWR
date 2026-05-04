eb9a668c8110c341a062067452730448f69c8610
tpiasek <tpiasekk@gmail.com>
2026-05-04 17:31:44 +0200

    First Sphinx docs config.

---
+++ docs/sphinx/source/client_api.rst
@@ -0,0 +1,11 @@
+Client API
+==========
+
+Send
+----
+
+
+
+Receive
+-------
+
---
+++ docs/sphinx/source/conf.py
@@ -0,0 +1,34 @@
+# conf.py
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../src'))
+
+# Configuration file for the Sphinx documentation builder.
+
+project = 'IoT-PWR Docs'
+copyright = '2026, tpiasek'
+author = 'tpiasek'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',          # generate docs from docstrings
+    'sphinx.ext.napoleon',         # Google/NumPy style docstrings handling
+    'sphinx_autodoc_typehints',    # types documentation from type hints
+    # 'breathe',                   # future doxygen integration
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
